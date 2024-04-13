a77cc9e9fef8e0dc4b60fd213e7d0d90c300c88d
mrsklg <klg.mrys@gmail.com>
2024-04-13 18:38:50 +0200

    Updated views and controllers

--- Atsumari/src/Controller/SecurityController.php
+++ Atsumari/src/Controller/SecurityController.php
@@ -4,23 +4,32 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'login')]
-    public function login(): Response
+    public function login(Request $request): Response
     {
-        return $this->render('security/login.html.twig', [
-            'text' => 'logowanie',
-        ]);
+        if ($request->isMethod('POST')) {
+            return $this->redirectToRoute('dashboard');
+        }
+        return $this->render('security/login.html.twig');
     }
 
     #[Route('/signup', name: 'signup')]
-    public function signup(): Response
+    public function signup(Request $request): Response
     {
-        return $this->render('security/signup.html.twig', [
-            'text' => 'rejestracja',
-        ]);
+        if ($request->isMethod('POST')) {
+            return $this->redirectToRoute('login');
+        }
+        return $this->render('security/signup.html.twig');
+    }
+
+    #[Route('/logout', name: 'logout')]
+    public function logout(): Response
+    {
+        return $this->redirectToRoute('start_page');
     }
 }
