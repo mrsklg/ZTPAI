12f5805c82dfbbd1b545f4fc6b6e27b961de3449
mrsklg <klg.mrys@gmail.com>
2024-05-26 17:13:03 +0200

    Installed encore

--- Atsumari/config/bundles.php
+++ Atsumari/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
 ];
